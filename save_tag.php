25109ed5ce0dd774a4f91ada08de7106a5f1f22e
Rednar Rosique <rednar64@gmail.com>
2013-10-05 04:41:00 -0500

    Num rows en el php

--- save_tag.php
+++ save_tag.php
@@ -8,11 +8,17 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-mysqli_query($con,"INSERT INTO Tags(tag) VALUES('$tag')");
+$queryS = mysqli_query($con,"SELECT * FROM Tags WHERE tag = '$tag'");
+$numRows = mysqli_num_rows($queryS);
+echo "numRows: ".numRows;
+printf("Result set has %d rows,\n",$numRows);
+
+if($numRows<=0)
+	mysqli_query($con,"INSERT INTO Tags(tag) VALUES('$tag')");
 
 mysqli_close($con);
-$gohere = "http://localhost/Dolphi/Home.html";
+//$gohere = "http://localhost/Dolphi/Home.html";
 
-header("Location: $gohere");
+//header("Location: $gohere");
 
 ?>
