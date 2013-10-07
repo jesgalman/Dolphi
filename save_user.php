f87add1540771bf8f83d60d756406ff748219246
Jesus Galindo Manrique <jesgalman@Jesuss-MacBook-Pro-3.local>
2013-10-06 20:04:52 -0500

    Version Hashbook presentacion Hackathon Facebook

--- save_user.php
+++ save_user.php
@@ -1,7 +1,5 @@
-<?php
-
-$id = $_REQUEST["id"];
-$tag = $_REQUEST["tag"];
+<?php 
+$u_id = $_GET["id"];
 
 $con=mysqli_connect("localhost","doscerod_hacker","hack2013","doscerod_Dolphi");
   
@@ -10,18 +8,14 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-$queryS = mysqli_query($con,"SELECT * FROM Tags WHERE tag = '$tag'");
+$queryS = mysqli_query($con,"SELECT * FROM Users WHERE u_id = '$u_id'");
 $numRows = mysqli_num_rows($queryS);
 
-if($numRows<=0){
-  mysqli_query($con,"INSERT INTO Tags(tag) VALUES('$tag')");
-  }
-
-mysqli_query($con, "INSERT INTO Users_Tags(u_id,t_id) VALUES('$id',(SELECT t_id FROM Tags WHERE tag = '$tag'))");
+if($numRows<=0)
+  mysqli_query($con,"INSERT INTO Users(u_id) VALUES('$u_id')");
 
 mysqli_close($con);
 $gohere = "http://hashbook.20d.mx/Home.html";
 
 header("Location: $gohere");
-
 ?>
